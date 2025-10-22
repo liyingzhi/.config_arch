23f3f36fa2a14a5b27242401c47aa8c9ea846b38
Fly_lilee <liyingli2018@gmail.com>
2025-10-22 13:35:32 +0800

    feat: set fd as path/file selector

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -12,6 +12,24 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
 --bind="ctrl-y:execute(readlink -f {} | echo -n {2..} | xclip -selection clipboard)"
 --bind="ctrl-alt-y:execute-silent(xclip -selection clipboard {})"
 '
+
+# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
+export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_CTRL_T_COMMAND='fd --type f'
+export FZF_ALT_C_COMMAND='fd --type f'
+
+
 # bind '"\C-x": "\C-r\C-m"'
 # bind '"\ex": "\C-r\C-m"'
 bindkey -s '^[x' '^R^M'
