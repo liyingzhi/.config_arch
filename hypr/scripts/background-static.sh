c5efdc2c84afd4e4d767e30d1c3660e09239039a
Fly_lilee <liyingli2018@gmail.com>
2026-01-04 14:33:44 +0800

    chore(hypr): comment out static background script execution

--- hypr/scripts/background-static.sh
+++ hypr/scripts/background-static.sh
@@ -7,6 +7,6 @@ do
     # Get a random wallpaper that is not the current one
     WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
     # Apply the selected wallpaper
-    hyprctl hyprpaper reload ,"$WALLPAPER"
+    hyprctl hyprpaper wallpaper " , $WALLPAPER"
     sleep 60
 done
