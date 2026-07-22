27359d055b7b1f06af7d3a87254c37220dd39acc
Fly_lilee <liyingli2018@gmail.com>
2026-07-22 12:54:19 +0800

    chore(hypr): add Emacs dir-locals to disable super-save

---
+++ hypr/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((lua-ts-mode . ((super-save-exclude . (".lua" ".conf"))))
+ (conf-unix-mode . ((super-save-exclude . (".conf")))))
