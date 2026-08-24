2bd4851868cccb7ea3a38e296a38451767c9d32e
Fly_lilee <liyingli2018@gmail.com>
2026-08-24 14:14:01 +0800

    fix(hypr-key): match and focus thunar class window case-insensitively

--- hypr/keybinds.lua
+++ hypr/keybinds.lua
@@ -31,7 +31,7 @@ hl.bind(mainMod .. " + CTRL + I", hl.dsp.exec_cmd(focus_or_raise("emacs", "[Ee]m
 hl.bind(mainMod .. " + CTRL + K", hl.dsp.exec_cmd(focus_or_raise(terminal, terminal, terminal)))
 hl.bind(mainMod .. " + CTRL + T", hl.dsp.exec_cmd(terminal))
 hl.bind(mainMod .. " + CTRL + F", hl.dsp.exec_cmd(focus_or_raise(browser, browser, browser)))
-hl.bind(mainMod .. " + CTRL + N", hl.dsp.exec_cmd(focus_or_raise(filemanager, "Thunar", filemanager)))
+hl.bind(mainMod .. " + CTRL + N", hl.dsp.exec_cmd(focus_or_raise(filemanager, "[Tt]hunar", filemanager)))
 hl.bind(mainMod .. " + SHIFT + T", hl.dsp.exec_cmd(terminal .. " btop"))
 
 -- Window management
